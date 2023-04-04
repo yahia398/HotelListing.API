31c1e70f858338ea32cfb4e2147e5e2f7cc5ef05
Yahia Saafan <yahiasaafan398@gmail.com>
2023-04-05 00:05:16 +0200

    Add Identity Tables and Roles

---
+++ HotelListing.API/Data/Configs/ApiUserConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelListing.API.Data.Configs
+{
+    public class ApiUserConfiguration : IEntityTypeConfiguration<ApiUser>
+    {
+        public void Configure(EntityTypeBuilder<ApiUser> builder)
+        {
+            
+            builder.Property(p => p.FirstName).HasColumnType("nvarchar(100)");
+            builder.Property(p => p.LastName).HasColumnType("varchar(100)");
+           
+        }
+    }
+
+    public class IdentityRoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+
+            builder.HasData(
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+                );
+
+        }
+    }
+}
