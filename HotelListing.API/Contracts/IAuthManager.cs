c1c82e10d4777c9d68a22eb37e2c9b81cfa8b9ec
Yahia Saafan <yahiasaafan398@gmail.com>
2023-04-05 04:59:42 +0200

    Create LoginDto Add LoginAsync Method to IAuthManager Interface Implement LoginAsync in AuthManager Class Create Login Endpoint in UsersController

--- HotelListing.API/Contracts/IAuthManager.cs
+++ HotelListing.API/Contracts/IAuthManager.cs
@@ -6,5 +6,6 @@ namespace HotelListing.API.Contracts
     public interface IAuthManager
     {
         Task<IEnumerable<IdentityError>> RegisterAsync(ApiUserDto apiUserDto);
+        Task<bool> LoginAsync(LoginDto loginDto);
     }
 }
